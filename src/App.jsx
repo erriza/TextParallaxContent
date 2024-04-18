4357a61f4279260752a651adf2fdd9a034e124f5
ricsae <eduricsa@hotmail.com>
2024-04-18 17:24:21 -0600

    subiendo parallax con react y tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { TextParallaxContentExample } from './components/TextParallaxContent'
+
+function App() {
+
+  return (
+    <>
+      <TextParallaxContentExample/>
+    </>
+  )
+}
+
+export default App
